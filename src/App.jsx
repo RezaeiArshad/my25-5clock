5726bf7a91f6c640669a57eb1e628afcc9f1801d
mehdi rezaei arshad <mehdirezaeiarshad@gmail.com>
2025-03-21 23:25:39 +0330

    It's done it just needs some debuging

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,18 @@
-import { useState } from 'react'
+import { useState, useContext, useEffect } from 'react'
 import './App.css'
-import { ScreenSizeProvider, useScreenSize } from './contexts';
+import { ScreenSizeProvider, useScreenSize, SessionProvider, SessionContext, BreakProvider, BreakContext, LockedProvider, IsLockedContext } from './contexts';
 
 function BreakTimer() {
+  const { breakx, setBreakx} = useContext(BreakContext)
   return (
     <div className="break-container">
       <h2>break duration</h2>
-      <div className="flex">
-       <button className="minus container-button">
+      <div className="flex can-be-locked">
+       <button onClick={() => setBreakx(breakx - 1)} className="minus container-button">
        reduce
       </button>
-      <h3>5 minutes</h3>
-      <button className="plus container-button">
+      <h3>{breakx}</h3>
+      <button onClick={() => setBreakx(breakx + 1)} className="plus container-button">
         increment
       </button>
       </div>
@@ -20,15 +21,17 @@ function BreakTimer() {
 }
 
 function SessionTimer() {
+  const { session, setSession} = useContext(SessionContext)
+
   return (
     <div className="session-container">
       <h2>session duration</h2>
-      <div className="flex">
-        <button className="minus container-button">
+      <div className="flex can-be-locked">
+        <button onClick={() => setSession(session - 1)} className="minus container-button">
           reduce
         </button>
-        <h3>5 minutes</h3>
-        <button className="plus container-button">
+        <h3>{session}</h3>
+        <button onClick={() => setSession(session + 1)} className="plus container-button">
           increment
         </button>
       </div>
@@ -37,18 +40,66 @@ function SessionTimer() {
 }
 
 function Timer() {
+  const {breakx, setBreakx} = useContext(BreakContext);
+  const {session, setSession} = useContext(SessionContext);
+  const [sessionTime, setSessionTime] = useState(session * 60)
+  const [breakTime, setBreakTime] = useState(breakx* 60);
+  const [isRunning, setIsRunning] = useState(false)
+  
+  const getTime = (timeInSeconds) => {
+    const timeMinute = Math.floor(timeInSeconds / 60);
+    const timeSeconds = timeInSeconds % 60 === 0 ? "00" : timeInSeconds % 60;
+    return `${timeMinute}:${("0" + timeSeconds).slice(-2)}`
+  }
+
+  const [onDash, setOnDash] = useState(getTime(sessionTime))
+
+  const start = () => {
+    setIsRunning(true);
+  }
+
+  const pause = () => {
+    setIsRunning(false)
+  }
+
+  const reset = () => {
+    setIsRunning(false);
+    setOnDash(getTime(session * 60))
+  }
+
+  useEffect(() => {
+    if (isRunning) {
+      setInterval(() => {
+      setSessionTime(sessionTime - 1)
+      setOnDash(getTime(sessionTime - 1))
+    }, 1000)
+    }
+      
+  }, [isRunning, sessionTime, breakTime, session, breakx])
+
   return (
     <div className="timer-container">
     <h2>having a <i className="italic">session</i></h2>
-    <h2>25:00</h2>
-    <button>play</button>
-    <button>pause</button>
-    <button>restart</button>
+    <h2 className="timer-numbers">{onDash}</h2>
+    <button onClick={start}>play</button>
+    <button onClick={pause}>pause</button>
+    <button onClick={reset}>restart</button>
     </div>
   )
 }
 
 function Dashboard() {
+
+  const {lock} = useContext(IsLockedContext)
+  useEffect(() => {
+
+    if (lock) document.querySelectorAll(".can-be-locked").forEach((div) => {
+      div.style.opacity = "0.3"
+    })
+    else document.querySelectorAll(".can-be-locked").forEach((div) => {
+      div.style.opacity = "1"
+    })
+  }, [])
   const {isMobile} = useScreenSize()
   return (
     <>
@@ -76,9 +127,16 @@ function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <ScreenSizeProvider>
-      <Dashboard />
-    </ScreenSizeProvider>
+    <LockedProvider>
+      <BreakProvider>
+        <SessionProvider>
+          <ScreenSizeProvider>
+            <Dashboard />
+          </ScreenSizeProvider>
+        </SessionProvider>
+      </BreakProvider>
+    </LockedProvider>
+    
   )
 }
 
--- src/contexts.jsx
+++ src/contexts.jsx
@@ -1,4 +1,4 @@
-import React, { createContext, useContext } from 'react';
+import React, { createContext, useContext, useState } from 'react';
 import { useMediaQuery } from 'react-responsive';
 
 const ScreenSizeContext = createContext();
@@ -12,5 +12,49 @@ export const ScreenSizeProvider = ({ children }) => {
         <ScreenSizeContext.Provider value={{isMobile}}>
             {children}
         </ScreenSizeContext.Provider>
+    )
+}
+
+export const SessionContext = createContext({
+    session: 25,
+    setSession: () => {}
+});
+
+export const SessionProvider = ({ children }) => {
+    const [session, setSession] = useState(25)
+    return (
+        <SessionContext.Provider value={{ session, setSession}}>
+            {children}
+        </SessionContext.Provider>
+    )
+}
+
+export const BreakContext = createContext({
+    breakx: 5,
+    setBreak: () => {}
+});
+
+
+export const BreakProvider = ({ children }) => {
+    const [breakx, setBreakx] = useState(5)
+    return (
+        <BreakContext.Provider value={{ breakx, setBreakx}}>
+            {children}
+        </BreakContext.Provider>
+    )
+}
+
+// islocked means the timer is ticking
+
+export const IsLockedContext = createContext({
+    isLocked: false
+})
+
+export const LockedProvider = ({ children }) => {
+    const [lock, setLock] = useState(false);
+    return (
+        <IsLockedContext.Provider value={{ lock, setLock }}>
+            {children}
+        </IsLockedContext.Provider>
     )
 }
